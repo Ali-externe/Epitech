4104451e65e6dea3da3ed301e68c4fe34b8af1a7
Yaich Ali <66366945+Ali-externe@users.noreply.github.com>
2021-09-03 17:54:13 +0200

    get_next_line repo

---
+++ First Year/CPE_getnextline_2017/get_next_line.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2017
+** getnextline
+** File description:
+** getnextline.h
+*/
+
+#ifndef GET_NEXT_LINE_H_
+#define GET_NEXT_LINE_H_
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#ifndef READ_SIZE
+#ifndef READ_SIZE
+#    define READ_SIZE (4)
+#endif
+
+#endif
